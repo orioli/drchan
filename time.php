77123dd6f9b828cda89c8651c766ce34e1211844
JOSE LOPEZ <jse@JOSEs-MacBook-Pro.local>
2017-06-27 15:14:56 -0700

    xx

--- time.php
+++ time.php
@@ -1,7 +1,8 @@
 <?php
 // https://help.chatfuel.com/facebook-messenger/plugins/json-plugin/
+// https://community.chatfuel.com/t/add-serverside-logic-to-your-bot-with-just-10-lines-of-javascript/3543
+
 $now = new DateTime();
 $text = array('text' => $now->format('Y-m-d H:i:s'));
-$messages = array('messages' => $text);
-echo json_encode($messages, JSON_FORCE_OBJECT);
+echo json_encode($text, JSON_FORCE_OBJECT);
 ?>
