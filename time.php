6869795f661a50f423aa8533e035c1f47b559a28
JOSE LOPEZ <jse@JOSEs-MacBook-Pro.local>
2017-06-27 15:23:04 -0700

    ewewew

--- time.php
+++ time.php
@@ -3,6 +3,5 @@
 // https://community.chatfuel.com/t/add-serverside-logic-to-your-bot-with-just-10-lines-of-javascript/3543
 
 $now = new DateTime();
-$text = array('text' => $now->format('Y-m-d H:i:s'));
-echo '{"messages":[ {"text":'.$text .'}]}'
+echo '{"messages":[ {"text":"'.$now->format('Y-m-d H:i:s .'"}]}'
 ?>
