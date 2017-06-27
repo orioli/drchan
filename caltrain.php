e07fb2d957945d9e23fba1df2228cb2c37e97356
JOSE LOPEZ <jse@JOSEs-MacBook-Pro.local>
2017-06-27 16:56:43 -0700

    wwww

---
+++ caltrain.php
@@ -0,0 +1,14 @@
+<?php
+// https://help.chatfuel.com/facebook-messenger/plugins/json-plugin/
+// https://community.chatfuel.com/t/add-serverside-logic-to-your-bot-with-just-10-lines-of-javascript/3543
+$html = file_get_html('view-source:http://www.caltrain.com/schedules/realtime/stations/22ndstreetstation-mobile.html');
+
+$ret = $html->find('div[class=ipf-st-ip-trains-subtable-td-arrivaltime]');
+// Find all images
+foreach($ret as $element)
+       echo $element . '<br>';
+
+$now = new DateTime();
+$thetext = $now->format('Y-m-d H:i:s');
+echo '{"messages":[ {"text":"'.$thetext .'"}]}'
+?>
