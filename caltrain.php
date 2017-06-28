04c3893ce182c14321b2520e220553cede5af32e
JOSE LOPEZ <jse@JOSEs-MacBook-Pro.local>
2017-06-27 17:00:12 -0700

    dsdsd

--- caltrain.php
+++ caltrain.php
@@ -3,7 +3,10 @@
 // https://community.chatfuel.com/t/add-serverside-logic-to-your-bot-with-just-10-lines-of-javascript/3543
 $html = file_get_html('view-source:http://www.caltrain.com/schedules/realtime/stations/22ndstreetstation-mobile.html');
 
-$ret = $html->find('div[class=ipf-st-ip-trains-subtable-td-arrivaltime]');
+
+//$ret = $html->find('div[class=ipf-st-ip-trains-subtable-td-arrivaltime]');
+
+$ret = $html->find('div.ipf-st-ip-trains-subtable-td-arrivaltime',0)->children(1)->outertext; 
 // Find all images
 foreach($ret as $element)
        echo $element . '<br>';
